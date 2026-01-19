e2ecaa7b4d08e2e5769ad04d6280b6b255db0898
Benjamin <oskidicey@gmail.com>
2026-01-19 14:12:22 -0800

    update config for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // This 'base' setting is required for GitHub Pages.
+  // If your repository name is different from 'lumina-press', 
+  // change the name between the slashes below to match it.
+  base: '/lumina-press/', 
 })
